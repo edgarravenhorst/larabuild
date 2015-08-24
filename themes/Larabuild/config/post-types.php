65fd4948aa31012e8dd344ed7cf9721e6fe3113c
Edgar Ravenhorst <edgarravenhorst@Edgars-iMac.local>
2015-08-24 09:25:13 +0200

    asd

--- themes/Larabuild/config/post-types.php
+++ themes/Larabuild/config/post-types.php
@@ -12,7 +12,7 @@ return array(
     "feature" => [
         "title" => "Features",
         "requiredParams" => [
-            "excerpt" => Data::createField("textarea", "Samenvatting", "Een korte omschrijving")
+            "excerpt" => Data::createField("textarea", "Samenvatting", "Een korte omschrijving"),
             "image" => Data::createField("text", "Afbeelding url", "Een afbeelding van google")
         ]
     ],
